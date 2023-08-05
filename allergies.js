7beec0ae273e025dedfbcf7fcf17e0ac7878e666
Pedro Henrique <pedrohenrique@MacBook-Air.local>
2023-08-05 16:36:06 -0300

    Estamos só comecando

--- allergies.js
+++ allergies.js
@@ -2,18 +2,34 @@
 // This is only a SKELETON file for the 'Allergies' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+const dicionario = {
+  eggs: 1,
+  peanuts: 2,
+  shellfish: 4,
+  strawberries: 8,
+  tomatoes: 16,
+  chocolate: 32,
+  pollen: 64,
+  cats: 128
+};
 
 export class Allergies {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  //instancia alergia com base no valor digitado 
+  constructor(alergia) {
+    this.identificadorAlergia = alergia;
   }
-
+  //lista que vai conter os itens no qual é alergico
   list() {
-    throw new Error('Remove this statement and implement this function');
+
   }
 
-  allergicTo() {
-    throw new Error('Remove this statement and implement this function');
+  allergicTo(alergico) {
+
+    if(this.identificadorAlergia === 0){
+      this.identificadorBinario = this.identificadorAlergia.toString(2)
+      console.log(this.identificadorAlergia.toString(2));
+      return false;
+    }
   }
 }
 
